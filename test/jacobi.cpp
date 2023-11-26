1fa9dd40f5235dd8942743af0bea7da7fd83e207
taldoflemis <coderflemis@gmail.com>
2023-11-25 23:42:32 -0300

    refactor: atualizar teste de jacobi para bater novos namespaces

--- test/jacobi.cpp
+++ test/jacobi.cpp
@@ -1,19 +1,19 @@
+#include "algebra/include/matrix.hpp"
+#include "algebra/include/vector.hpp"
 #include "models/include/gauss_jacobi.hpp"
-#include "models/include/matrix.hpp"
-#include "models/include/vector.hpp"
 #include <gtest/gtest.h>
 #include <memory>
 
 TEST(JacobiTest, SlideAssertion) {
-  Matrix m{{{10, 2, 1}, {1, 5, 1}, {2, 3, 10}}};
-  Vector v{7, -8, 6};
-  auto jacobi = std::make_unique<GaussJacobi>();
+  Algebra::Matrix m{{{10, 2, 1}, {1, 5, 1}, {2, 3, 10}}};
+  Algebra::Vector v{7, -8, 6};
+  auto jacobi = std::make_unique<Models::GaussJacobi>();
   double error = 0.0001;
 
   jacobi->solve(m, v, error, 1000);
 
-  Vector res = jacobi->getResult();
-  Vector expected{1, -2, 1};
+  Algebra::Vector res = jacobi->getResult();
+  Algebra::Vector expected{1, -2, 1};
 
   for (int i = 0; i < res.getSize(); i++) {
     EXPECT_NEAR(res.getValue(i), expected.getValue(i), error);
