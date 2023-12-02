4959dfc3991b7516c6b1601b98952a25bb8b1947
taldoflemis <coderflemis@gmail.com>
2023-12-02 17:47:52 -0300

    tests: adicionar mais testes para seidel e jacobi

--- test/jacobi.cpp
+++ test/jacobi.cpp
@@ -19,3 +19,22 @@ TEST(JacobiTest, SlideAssertion) {
     EXPECT_NEAR(res.getValue(i), expected.getValue(i), error);
   }
 }
+
+TEST(JacobiTest, FourByFour) {
+  Algebra::Matrix m{
+      {5, -1, 0, 0}, {-1, 5, -1, 0}, {0, -1, 5, -1}, {0, 0, -1, 5}};
+
+  Algebra::Vector v{2, 5, -4, 6};
+  auto jacobi = std::make_unique<Models::GaussJacobi>();
+
+  double error = 0.0001;
+
+  jacobi->solve(m, v, error, 1000);
+
+  Algebra::Vector res = jacobi->getResult();
+  Algebra::Vector expected{336.0 / 551, 578.0 / 551, -201.0 / 551, 621.0 / 551};
+
+  for (int i = 0; i < res.getSize(); i++) {
+    EXPECT_NEAR(res.getValue(i), expected.getValue(i), error);
+  }
+}
--- test/seidel.cpp
+++ test/seidel.cpp
@@ -5,17 +5,36 @@
 #include <memory>
 
 TEST(SeidelTest, SlideAssertion) {
-    Algebra::Matrix m{{{10, 2, 1}, {1, 5, 1}, {2, 3, 10}}};
-    Algebra::Vector v{7, -8, 6};
-    auto seidel = std::make_unique<Models::GaussSeidel>();
-    double error = 0.0001;
-    
-    seidel->solve(m, v, error, 1000);
-    
-    Algebra::Vector res = seidel->getResult();
-    Algebra::Vector expected{1, -2, 1};
-    
-    for (int i = 0; i < res.getSize(); i++) {
-        EXPECT_NEAR(res.getValue(i), expected.getValue(i), error);
-    }
+  Algebra::Matrix m{{{10, 2, 1}, {1, 5, 1}, {2, 3, 10}}};
+  Algebra::Vector v{7, -8, 6};
+  auto seidel = std::make_unique<Models::GaussSeidel>();
+  double error = 0.0001;
+
+  seidel->solve(m, v, error, 1000);
+
+  Algebra::Vector res = seidel->getResult();
+  Algebra::Vector expected{1, -2, 1};
+
+  for (int i = 0; i < res.getSize(); i++) {
+    EXPECT_NEAR(res.getValue(i), expected.getValue(i), error);
+  }
+}
+
+TEST(SeidelTest, FourByFour) {
+  Algebra::Matrix m{
+      {5, -1, 0, 0}, {-1, 5, -1, 0}, {0, -1, 5, -1}, {0, 0, -1, 5}};
+
+  Algebra::Vector v{2, 5, -4, 6};
+  auto seidel = std::make_unique<Models::GaussSeidel>();
+
+  double error = 0.0001;
+
+  seidel->solve(m, v, error, 1000);
+
+  Algebra::Vector res = seidel->getResult();
+  Algebra::Vector expected{336.0 / 551, 578.0 / 551, -201.0 / 551, 621.0 / 551};
+
+  for (int i = 0; i < res.getSize(); i++) {
+    EXPECT_NEAR(res.getValue(i), expected.getValue(i), error);
+  }
 }
